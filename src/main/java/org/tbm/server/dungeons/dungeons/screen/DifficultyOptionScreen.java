c1588a61aac201e6d5970eb8c7e4626246050586
Skullian <133133667+Skullians@users.noreply.github.com>
2023-07-20 16:15:31 +0100

    switched to data-driven ui (fixed discrepancies Gladge)

--- src/main/java/org/tbm/server/dungeons/dungeons/screen/DifficultyOptionScreen.java
+++ src/main/java/org/tbm/server/dungeons/dungeons/screen/DifficultyOptionScreen.java
@@ -1,17 +1,15 @@
 package org.tbm.server.dungeons.dungeons.screen;
 
-import io.wispforest.owo.ui.base.BaseOwoScreen;
+import io.wispforest.owo.ui.base.BaseUIModelScreen;
 import io.wispforest.owo.ui.component.ButtonComponent;
-import io.wispforest.owo.ui.component.Components;
+import io.wispforest.owo.ui.component.ItemComponent;
 import io.wispforest.owo.ui.component.LabelComponent;
-import io.wispforest.owo.ui.container.Containers;
 import io.wispforest.owo.ui.container.FlowLayout;
-import io.wispforest.owo.ui.core.*;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.item.Items;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
-import org.jetbrains.annotations.NotNull;
+import net.minecraft.util.Identifier;
 import org.tbm.server.dungeons.dungeons.Dungeons;
 import org.tbm.server.dungeons.dungeons.component.IDifficultyComponent;
 import org.tbm.server.dungeons.dungeons.component.ILastUpdatedDifficultyComponent;
@@ -23,95 +21,76 @@ import java.util.Date;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
 
-public class DifficultyOptionScreen extends BaseOwoScreen<FlowLayout> {
+public class DifficultyOptionScreen extends BaseUIModelScreen<FlowLayout> {
 
     String[] difficulties = {"Easy", "Normal", "Hard", "Extreme"};
     public final MinecraftClient playerMc = MinecraftClient.getInstance();
     String[] descriptions = {
-            "§l- Constant Resistance 1 to player.\n§l- 0.7x Coin Drops Multiplier\n§l- 0.7x Professions XP Multiplier",
-            "§l- No Buffs or Debuffs\n§l- 1x Coin Drops Multiplier\n§l- 1x Professions XP Multiplier",
-            "§l- 7 Hearts\n§l- 1.5x Coin Drops Multiplier\n§l- 1.5x Professions XP Multiplier",
-            "§l- 5 Hearts\n§l- 2x Coin Drops Multiplier\n§l- 2x Professions XP Multiplier",
+            "§l- EASY MODE (line1)\n§l- Line 2\n§l- Line 3\n§l- Line 4",
+            "§l- NORMAL MODE (line1)\n§l- Line 2\n§l- Line 3\n§l- Line 4",
+            "§l- HARD MODE (line1)\n§l- Line 2\n§l- Line 3\n§l- Line 4",
+            "§l- EXTREME MODE (line1)\n§l- Line 2\n§l- Line 3\n§l- Line 4",
     };
 
     AtomicLong lastUpdate = new AtomicLong(0);
 
     AtomicInteger pos = new AtomicInteger(1);
 
-    @Override
-    protected @NotNull OwoUIAdapter<FlowLayout> createAdapter() {
+    public DifficultyOptionScreen() {
+        super(FlowLayout.class, DataSource.asset(new Identifier("tbm_dungeons", "difficultyoptionscreen")));
         if (playerMc.player != null) {
             IDifficultyComponent difficulty = ModComponents.DIFFICULTY_SETTING.get(playerMc.player);
             ILastUpdatedDifficultyComponent lastUpdated = ModComponents.LAST_UPDATED.get(playerMc.player);
             pos.set(difficulty.getValue());
             lastUpdate.set(lastUpdated.getValue());
         }
-        return OwoUIAdapter.create(this, Containers::verticalFlow);
     }
+
+
     @Override
     protected void build(FlowLayout rootComponent) {
-        rootComponent
-                .surface(Surface.VANILLA_TRANSLUCENT)
-                .horizontalAlignment(HorizontalAlignment.CENTER)
-                .verticalAlignment(VerticalAlignment.CENTER);
 
-
-        rootComponent.child(
-                Containers.verticalFlow(Sizing.content(), Sizing.content())
-                        .child(Components.box(Sizing.fill(70), Sizing.fill(60)))
-                        .padding(Insets.of(4))
-                        .surface(Surface.DARK_PANEL)
-                        .verticalAlignment(VerticalAlignment.CENTER)
-                        .horizontalAlignment(HorizontalAlignment.CENTER)
-                        .positioning(Positioning.relative(50, 50))
-        );
-
-        rootComponent.child(
-                Containers.verticalFlow(Sizing.content(), Sizing.content())
-                        .child(Components.label(Text.literal("Difficulty Settings").formatted(Formatting.WHITE)))
-                        .verticalAlignment(VerticalAlignment.CENTER)
-                        .horizontalAlignment(HorizontalAlignment.CENTER)
-                        .sizing(Sizing.content(), Sizing.content())
-                        .positioning(Positioning.relative(50, 10))
-        );
-
-        rootComponent.child(
-                Components.label(Text.literal(descriptions[pos.get()]).formatted(Formatting.BOLD))
-                        .sizing(Sizing.content(), Sizing.content())
-                        .positioning(Positioning.relative(50, 45))
-                        .id("description")
-        );
-
+        // pre-setting descriptions / difficulty button peepoRun
         LabelComponent label = rootComponent.childById(LabelComponent.class, "description");
+        label.text(Text.literal(descriptions[pos.get()]));
 
-        rootComponent.child(
-                Components.button(Text.literal(difficulties[pos.get()]).formatted(Formatting.BOLD), (ButtonComponent button) -> {
-                            if (pos.get() == 3) {
-                                pos.set(0);
-                            } else {
-                                pos.getAndIncrement();
-                            }
-                            button.setMessage(Text.literal(difficulties[pos.get()]).formatted(Formatting.BOLD));
-                            button.setWidth(56);
-                            label.text(Text.literal(descriptions[pos.get()]));
-                        })
-                        .positioning(Positioning.relative(50, 28))
-                        .sizing(Sizing.fixed(56), Sizing.fixed(20))
-                        .id("difficulty")
-        );
+        ButtonComponent difficultyButton = rootComponent.childById(ButtonComponent.class, "difficultybutton");
+        difficultyButton.setMessage(Text.literal(difficulties[pos.get()]).formatted(Formatting.BOLD));
+
+        // clock item display + 7 day time notifier (i had to put the components in the xml file instead of having just the two components as code-driven,
+        // but it would cause it to go whack if it was code driven with different ui scales so i just shoved it in there instead !vanish peepoRun
+        ItemComponent clock = rootComponent.childById(ItemComponent.class, "clock");
+        LabelComponent timelimit = rootComponent.childById(LabelComponent.class, "time");
+        ButtonComponent confirmbutton = rootComponent.childById(ButtonComponent.class, "confirm_difficulty");
+
+
+        // click listeners Gladge
+
+        rootComponent.childById(ButtonComponent.class, "difficultybutton").onPress(button -> {
+            if (pos.get() == 3) {
+                pos.set(0);
+            } else {
+                pos.getAndIncrement();
+            }
+            button.setMessage(Text.literal(difficulties[pos.get()]).formatted(Formatting.BOLD));
+            button.setWidth(56);
+            label.text(Text.literal(descriptions[pos.get()]).formatted(Formatting.BOLD));
+        });
+
+        // 7 day restrictions (ily slow for the code)
+
         if (System.currentTimeMillis() - lastUpdate.get() < 604800000) {
-            rootComponent.child(Components.item(Items.CLOCK.getDefaultStack()).sizing(Sizing.fixed(12)).positioning(Positioning.relative(50, 70)));
+            confirmbutton.visible = false;
+            clock.stack(Items.CLOCK.getDefaultStack());
             Date currentDate = new Date(lastUpdate.get() + 604800000);
-            rootComponent.child(Components.label(Text.literal("Next change available on: " + DateFormat.getInstance().format(currentDate)).formatted(Formatting.WHITE)).positioning(Positioning.relative(50, 75)));
+            timelimit.text(Text.literal("Next change available on: " + DateFormat.getInstance().format(currentDate)).formatted(Formatting.WHITE));
         } else {
-            rootComponent.child(
-                Components.button(Text.literal("Done").formatted(Formatting.BOLD), (ButtonComponent button) -> {
-                            Dungeons.STATE_CHANNEL.clientHandle().send(new C2SDifficultyUpdate(pos.get()));
-                            close();
-                        })
-                        .sizing(Sizing.fixed(56), Sizing.fixed(20))
-                        .positioning(Positioning.relative(50, 70))
-            );
+            confirmbutton.setMessage(Text.literal("Done").formatted(Formatting.BOLD));
+            confirmbutton.onPress(button -> {
+                confirmbutton.visible = true;
+                Dungeons.STATE_CHANNEL.clientHandle().send(new C2SDifficultyUpdate(pos.get()));
+                close();
+            });
         }
     }
 }
