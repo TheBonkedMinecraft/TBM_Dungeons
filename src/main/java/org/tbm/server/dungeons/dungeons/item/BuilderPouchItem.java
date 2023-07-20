ebc346f873d74c0bf939b49fa5f8bf51d0cbb8b2
Bhavya Singh <bhavyasingh2611@outlook.com>
2023-07-20 20:29:41 +0530

    Uncrashify

---
+++ src/main/java/org/tbm/server/dungeons/dungeons/item/BuilderPouchItem.java
@@ -0,0 +1,9 @@
+package org.tbm.server.dungeons.dungeons.item;
+
+import net.minecraft.item.Item;
+
+public class BuilderPouchItem extends Item {
+    public BuilderPouchItem(Settings settings) {
+        super(settings);
+    }
+}
--- src/main/java/org/tbm/server/dungeons/dungeons/item/ModItems.java
+++ src/main/java/org/tbm/server/dungeons/dungeons/item/ModItems.java
@@ -13,11 +13,11 @@ import org.tbm.server.dungeons.dungeons.Dungeons;
 public class ModItems {
     public static final Item DUNGEON_CRYSTAL = registerItem("dungeon_crystal",
             new Item(new FabricItemSettings().fireproof().group(ItemGroup.MATERIALS).maxCount(1).rarity(Rarity.EPIC)));
+    public static final Item BUILDER_POUCH = registerItem("builder_pouch", new Item(new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS).maxCount(1).rarity(Rarity.COMMON)));
     public static final Item HEART_OF_ATTRACTION = registerItem("heart_of_attraction",
             new Item(new FabricItemSettings().fireproof().group(ItemGroup.MATERIALS).maxCount(64).rarity(Rarity.RARE).food(FoodComponents.GOLDEN_APPLE)));
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registry.ITEM, new Identifier(Dungeons.MOD_ID, name), item);
     }
-    public static void registerModItems() {
-    }
+    public static void registerModItems() {}
 }
